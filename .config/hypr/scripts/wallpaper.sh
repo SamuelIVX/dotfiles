93bb76320d1678e518d3ae3999de5bb5be0f8e9e
Samuel <samuel05.hb@gmail.com>
2026-01-04 22:11:28 -0500

    Updated wallpaper / avatar file paths.

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -7,19 +7,19 @@ workspace=$(hyprctl activeworkspace -j | jq -r '.id')
 # Set wallpapers for each workspace
 case $workspace in
     1)
-        awww img /home/samhb/Downloads/csm-wallpaper.jpg --transition-type=wave --transition-duration=1
+        awww img /home/samhb/Pictures/Wallpapers/csm_wallpaper.jpg --transition-type=wave --transition-duration=1
         ;;
     2)
-        awww img /home/samhb/Downloads/reze.png --transition-type=any --transition-duration=1
+        awww img /home/samhb/Pictures/Wallpapers/reze_wallpaper.png --transition-type=any --transition-duration=1
         ;;
     3)
-        awww img /home/samhb/Downloads/dandadan_wallpaper.png --transition-type=grow --transition-duration=1
+        awww img /home/samhb/Pictures/Wallpapers/dandadan_wallpaper.png --transition-type=grow --transition-duration=1
         ;;
     4)
-        awww img /home/samhb/Downloads/goku_black_wallpaper.jpg --transition-type=wipe --transition-duration=1
+        awww img /home/samhb/Pictures/Wallpapers/goku_black_wallpaper.jpg --transition-type=wipe --transition-duration=1
         ;;
     5)
-        awww img --transition-type=outer --transition-pos 0.854,0.977 --transition-step 90 /home/samhb/Downloads/evangelion_wallpaper.png
+        awww img --transition-type=outer --transition-pos 0.854,0.977 --transition-step 90 /home/samhb/Pictures/Wallpapers/evangelion_wallpaper.png
         ;;
 esac
 
